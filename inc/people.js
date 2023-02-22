0f9aca53b12f1f6124482690c881399659389abd
Chris7073 <93035408+Chris7073@users.noreply.github.com>
2023-02-22 11:18:50 +0100

    Update people.js

--- inc/people.js
+++ inc/people.js
@@ -7,8 +7,20 @@ export let names = [
     }, 
     {
         name: "Chris7073",
-        emoji: "",
-        background_color: 1,
-        foreground_color: 1
+        emoji: "🌚",
+        background_color: "#65A2CD",
+        foreground_color: "#265475"
+    },
+    {
+        name: "Kurro_Wolfy",
+        emoji: "💜",
+        background_color: "#ef56d1",
+        foreground_color: "#1de278"
+    },
+    {
+        name: "VikkiVuk",
+        emoji: "🐺",
+        background_color: "#083ad1",
+        foreground_color: "#c5e8df"
     }
 ]
