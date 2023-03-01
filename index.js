39da23ceb8d2237faaf0fe78aecc7c2537382c46
Chris7073 <cicciarellachristian@gmail.com>
2023-03-01 22:28:02 +0100

    Update

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ let contenitore = document.getElementById("wrap");
 function getNames() {
     names.forEach(element => {
         let utente = document.createElement("div");
-        //utente.innerHTML = element.emoji + (element.name.length > 10 ? `${element.name.substring(0, 10)}...` : element.name);
-        utente.innerHTML = element.emoji + element.name;
+        let width = window.innerWidth;
+        utente.innerHTML = element.emoji + (element.name.length > 12 ? `${element.name.substring(0, width <600?12:16)}...` : element.name);
         contenitore.appendChild(utente);
         utente.classList.add("bolla");
-        utente.style.fontSize = element.name.length > 12? (window.innerWidth <640 ?"10px":"12px"):"16px";
         utente.style.backgroundColor = element.background_color == 1 ? "#dee3ed" : element.background_color;
         utente.style.color = element.foreground_color == 1 ? "#000000bd" : "#ffffffbd";
     });
