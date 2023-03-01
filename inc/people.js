7e7dff6d03a9d726dd673ea3707514a5d5222f1f
Chris7073 <cicciarellachristian@gmail.com>
2023-03-01 22:17:07 +0100

    Update all

--- inc/people.js
+++ inc/people.js
@@ -1,3 +1,4 @@
+// colore 1: NERO - 2: BIANCO
 export let names = [
     {
         name: "Sumeet",
@@ -7,6 +8,48 @@ export let names = [
     }, 
     {
         name: "Chris7073",
+        emoji: "🌚",
+        background_color: "#65A2CD",
+        foreground_color: 1
+    },
+    {
+        name: "Kurro_Wolfy",
+        emoji: "💜",
+        background_color: "#ef56d1",
+        foreground_color: 2
+    },
+    {
+        name: "VikkiVuk",
+        emoji: "🐺",
+        background_color: "#083ad1",
+        foreground_color: 2
+    },
+    {
+        name: "Just_A_Mangled_Corpse.exe",
+        emoji: "",
+        background_color: 1,
+        foreground_color: 1
+    }, 
+    {
+        name: "MatB57",
+        emoji: "",
+        background_color: 1,
+        foreground_color: 1
+    }, 
+    {
+        name: "mosalah",
+        emoji: "",
+        background_color: 1,
+        foreground_color: 1
+    }, 
+    {
+        name: "PiweeeX",
+        emoji: "",
+        background_color: 1,
+        foreground_color: 1
+    }, 
+    {
+        name: "Sketchware Expert",
         emoji: "",
         background_color: 1,
         foreground_color: 1
--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import { names } from './inc/people.js';
 
 let contenitore = document.getElementById("wrap");
-
-async function getNames() {
-    console.log("Loading wall");
-    await names.forEach(element => {
+function getNames() {
+    names.forEach(element => {
         let utente = document.createElement("div");
+        //utente.innerHTML = element.emoji + (element.name.length > 10 ? `${element.name.substring(0, 10)}...` : element.name);
         utente.innerHTML = element.emoji + element.name;
         contenitore.appendChild(utente);
         utente.classList.add("bolla");
-        utente.style.backgroundColor = element.background_color != 1 ? element.background_color : "#dee3ed";
-        utente.style.color = element.background_color != 1 ? element.foreground_color : "#2f3237";
+        utente.style.fontSize = element.name.length > 12? (window.innerWidth <640 ?"10px":"12px"):"16px";
+        utente.style.backgroundColor = element.background_color == 1 ? "#dee3ed" : element.background_color;
+        utente.style.color = element.foreground_color == 1 ? "#000000bd" : "#ffffffbd";
     });
-    await console.log("Loaded %s names", names.length);
 }
 
 getNames();
